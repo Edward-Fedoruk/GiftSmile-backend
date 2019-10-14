288d30872d8b08b7a8328baaaf31be27e4fc5021
ed.fedorukk@gmail.com <nina.bondar.cs@gmail.com>
2019-10-14 23:35:42 +0300

    config

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -1,37 +1,37 @@
-const jwt = require('jsonwebtoken')
-const { prop, or, slice, startsWith, pipe, otherwise, then } = require('ramda')
+const jwt = require('jsonwebtoken');
+const {
+  prop, or, slice, startsWith, pipe, otherwise, then,
+} = require('ramda');
 
-const secret = 'worldisfullofdevelopers'
+const secret = 'worldisfullofdevelopers';
 
-const getOneOfAccessProp = o => or(prop('x-access-token', o), prop('authorization', o))
+const getOneOfAccessProp = (o) => or(prop('x-access-token', o), prop('authorization', o));
 
-const verifyWithSecret = token => new Promise(
-	(resolve, reject) => 
-		jwt.verify(token, secret, 
-			(err, decoded) => err 
-				? reject({ success: false, message: 'Token is not valid' }) : resolve(decoded))
-)
+const verifyWithSecret = (token) => new Promise(
+  (resolve, reject) => jwt.verify(token, secret,
+    (err, decoded) => (err
+      ? reject({ success: false, message: 'Token is not valid' }) : resolve(decoded))),
+);
 
-const success = (req, next) => decoded => {
-	req.decoded = decoded
-	next()
-}
+const success = (req, next) => (decoded) => {
+  req.decoded = decoded;
+  next();
+};
 
 const checkToken = (req, res, next) => {
-	const handleVerified = success(req, next)
+  const handleVerified = success(req, next);
 
-	const verifyToken = pipe(
-		slice(7, Infinity),
-		verifyWithSecret,
-		then(handleVerified),
-		otherwise(res.json)
-	)
+  const verifyToken = pipe(
+    slice(7, Infinity),
+    verifyWithSecret,
+    then(handleVerified),
+    otherwise(res.json),
+  );
 
-	const accessProp = getOneOfAccessProp(req.headers)
-	console.log(accessProp)
-	startsWith('Bearer ', accessProp)
-		? verifyToken(accessProp)
-		: res.json({ success: false, message: 'Auth token is not supplied' })
-}
+  const accessProp = getOneOfAccessProp(req.headers);
+  startsWith('Bearer ', accessProp)
+    ? verifyToken(accessProp)
+    : res.json({ success: false, message: 'Auth token is not supplied' });
+};
 
-module.exports = checkToken
+module.exports = checkToken;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,67 @@
-// @flow
-import type {
-  $Request,
-  $Response,
-  NextFunction,
-  Middleware,
-} from 'express';
+require('dotenv').config()
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
+const db = require('./db')
+const checkToken = require('./middlewares/checkToken.js')
 
-const secret: string = "worldisfullofdevelopers"
+const secret = 'worldisfullofdevelopers'
+
+class HandlerGenerator {
+  login(req, res) {
+    const { username } = req.body
+    const { password } = req.body
+    // For the given username fetch user from DB
+    const mockedUsername = 'admin'
+    const mockedPassword = 'password'
+    if (username && password) {
+      if (username === mockedUsername && password === mockedPassword) {
+        const token = jwt.sign(
+          { username },
+          secret,
+          { expiresIn: '24h' },
+        )
+        // return the JWT token for the future API calls
+        res.json({
+          success: true,
+          message: 'Authentication successful!',
+          token,
+        })
+      } else {
+        res.sendStatus(403).json({
+          success: false,
+          message: 'Incorrect username or password',
+        })
+      }
+    } else {
+      res.sendStatus(400).json({
+        success: false,
+        message: 'Authentication failed! Please check the request',
+      })
+    }
+  }
+
+  index(req, res) {
+    res.json({
+      success: true,
+      message: 'Index page',
+    })
+  }
+}
+
+// Starting point of the server
+function main() {
+  const app = express() // Export app for other routes to use
+  const handlers = new HandlerGenerator()
+  const port = process.env.PORT || 5000
+  app.use(bodyParser.urlencoded({ // Middleware
+    extended: true,
+  }))
+  app.use(bodyParser.json())
+  // Routes & Handlers
+  app.post('/login', handlers.login)
+  app.get('/', checkToken, handlers.index)
+  app.listen(port, () => console.log(`Server is listening on port: ${port}`))
+}
+
+main()
